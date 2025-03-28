830f5c58f43128804d0e26ebbd1fad89cb98f2b5
Aleksandra Balashova <ealba003@edu.xamk.fi>
2025-03-28 15:00:59 +0200

    controller and midleware done

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routers/auth.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.use("/api/auth", authRoutes);
+
+export default app;
