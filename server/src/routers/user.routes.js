a84eff9bbe58062ea58a427f18dd142d7c07df1b
Aleksandra Balashova <ealba003@edu.xamk.fi>
2026-03-28 19:41:52 +0200

    fixed the routes

--- server/src/routers/user.routes.js
+++ server/src/routers/user.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { getUsers } from "../controllers/user.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js";
+import { authenticateToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", authMiddleware, getUsers);
+router.get("/", authenticateToken, getUsers);
 
 export default router;
